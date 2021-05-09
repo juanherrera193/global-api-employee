1d461fd1e757dfda7a0d2591a5fbaf415645f206
Juan Pablo <pabliny193@gmail.com>
2021-05-08 22:22:11 -0500

    Add CrossOrigin annotation to allow consumption from react

--- src/main/java/com/masglobal/employee/controller/EmployeeController.java
+++ src/main/java/com/masglobal/employee/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping(value = "/employees")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class EmployeeController {
 
   private final EmployeeService employeeService;
